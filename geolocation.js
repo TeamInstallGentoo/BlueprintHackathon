34f208992ccdc40adf428e1e9662236afdc9bba0
Andrew Schembor <schembora@gmail.com>
2015-02-22 15:20:58 -0500

    Added button and js file

---
+++ geolocation.js
@@ -0,0 +1,21 @@
+function geoFindMe() {
+  var output = document.getElementById("out");
+
+  if (!navigator.geolocation){
+    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+    return;
+  }
+
+  function success(position) {
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    "<p> Hi </p>"
+  };
+
+  function error() {
+    output.innerHTML = "Unable to retrieve your location";
+  };
+
+  output.innerHTML = "<p>Locating…</p>";
+
+  navigator.geolocation.getCurrentPosition(success, error);
